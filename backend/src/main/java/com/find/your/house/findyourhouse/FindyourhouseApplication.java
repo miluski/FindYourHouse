b99f64f28f9705dd1ed16dd235492eb8b0e43af5
sprytnykowal@gmail.com <sprytnykowal@gmail.com>
2024-03-11 16:58:51 +0100

    Initialize backend and frontend project structure

---
+++ backend/src/main/java/com/find/your/house/findyourhouse/FindyourhouseApplication.java
@@ -0,0 +1,13 @@
+package com.find.your.house.findyourhouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FindyourhouseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FindyourhouseApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/find/your/house/findyourhouse/FindyourhouseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.find.your.house.findyourhouse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FindyourhouseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
