0b45ce401e956c536fdd398e66a51c431a00674f
JacobJS7 <j.szczur@interia.pl>
2024-06-20 23:41:00 +0200

    Fix in GuardView component

--- frontend/src/views/ErrorViews/GuardView.tsx
+++ frontend/src/views/ErrorViews/GuardView.tsx
@@ -4,9 +4,8 @@ import { Navigate } from "react-router-dom";
 export default function GuardView(props: { children: ReactElement }) {
 	const token = localStorage.getItem("token");
 	return token !== "" && token !== null ? (
-		
-		<Navigate to='/unathorized' />
-	) : (
 		props.children
+	) : (
+		<Navigate to='/unathorized' />
 	);
 }
